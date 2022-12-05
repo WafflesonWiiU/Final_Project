f48a76d946ba181e0e090dd3a9cb3b4cadfbef90
Ayden Budish <ab44990@ALA.local>
2022-12-05 09:05:59 -0700

    starter js code

---
+++ assets/js/java.js
@@ -0,0 +1,9 @@
+var imageVisibilty = true;
+var img 
+    function showImages(){
+       imageVisibilty = false;
+       if (imageVisibilty == false) {
+        var img = document.getElementById('productImages');
+        img.style.visibility = 'visible';
+       }
+      }
