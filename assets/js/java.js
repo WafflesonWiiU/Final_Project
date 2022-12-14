4a019ce3b19ef9c63ba521cc934d3389927688ec
Ayden Budish <ab44990@ALA.local>
2022-12-14 08:40:49 -0700

    guessing game

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,4 +1,5 @@
 
+//hide images when user clicks button
 function hideImages() {
     var x = document.getElementById("productImages");
     if (x.style.display === "none") {
@@ -7,6 +8,16 @@ function hideImages() {
       x.style.display = "none";
     }
   }
+
+  //Check if user was correct in their guess
   function guessCheck(){
+    if (z == document.getElementById("numberInput").value){
+      console.log ("good")
+    }
     
+  }
+  var z = Math.floor(Math.random() * 100)
+//generate a number
+  function Rng(){
+     z = Math.floor(Math.random() * 100)
   }
